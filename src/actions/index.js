1016d26e652463221e204e11a2b600ce4dd45550
comseung18 <comseung18@koreatech.ac.kr>
2020-12-01 20:58:52 +0900

    ho

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,11 @@
+import { createAction } from '@reduxjs/toolkit'
+
 let nextTodoId = 0
-export const addTodo = text => ({
-  type: 'ADD_TODO',
-  id: nextTodoId++,
-  text
-})
+export const addTodo = createAction('ADD_TODO', text => {
+  return {
+    payload: { id: nextTodoId++, text }
+  }
+});
 
 export const setVisibilityFilter = filter => ({
   type: 'SET_VISIBILITY_FILTER',
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux'
 import App from './components/App'
 import rootReducer from './reducers'
+import { addTodo } from './actions/index'
 
 const store = configureStore({
   reducer: rootReducer
 });
+
+const myAction = addTodo('빵먹기');
+console.log(myAction);
 
 render(
   <Provider store={store}>
